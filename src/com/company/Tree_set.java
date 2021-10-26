9fa753f7d47314ac1cd5ae713ce8e3607db34747
Dasha <dashanecheporenko2161@gmail.com>
2021-10-26 11:31:07 +0300

    Initial commit

---
+++ src/com/company/Tree_set.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.TreeSet;
+
+public class Tree_set {
+    public TreeSet<String> treeSet;
+    public Tree_set(String[] strings)
+    {
+        this.treeSet = new TreeSet<String>();
+        this.add(strings);
+    }
+
+    public void add(String[] strings)
+    {
+        for (int i = 0; i < strings.length; i++)
+        {
+            this.treeSet.add(strings[i]);
+        }
+    }
+    public TreeSet getTreeSet() {
+        return treeSet;
+    }
+}
